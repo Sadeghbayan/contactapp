e9325324e3f00649b52f3aebc3a149c935769136
Soheil Sadeghbayan <soheilsadeghbayan@yahoo.com>
2020-01-01 02:50:41 +0330

    Updates

---
+++ precache-manifest.c69c3b3d79dfa8c5365b9ec0e282694e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4635af3f5f1a1e665bd7b2eb2664ec",
+    "url": "/contactapp/index.html"
+  },
+  {
+    "revision": "c327e1cd1f300cd8b5fe",
+    "url": "/contactapp/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "8aebdbbd008ffd4e2d10",
+    "url": "/contactapp/static/css/main.1737b5cc.chunk.css"
+  },
+  {
+    "revision": "c327e1cd1f300cd8b5fe",
+    "url": "/contactapp/static/js/2.3ae3e9c6.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/contactapp/static/js/2.3ae3e9c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8aebdbbd008ffd4e2d10",
+    "url": "/contactapp/static/js/main.e1f7d961.chunk.js"
+  },
+  {
+    "revision": "fa9bac0ab9e1c26876ab",
+    "url": "/contactapp/static/js/runtime-main.43634af0.js"
+  }
+]);
